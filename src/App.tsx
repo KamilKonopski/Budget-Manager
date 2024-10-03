28bd29e58fe8f1545ddf8d32b6f2d0c75585bfad
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2024-10-04 00:17:19 +0200

    add routing for differents views

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
-import Layout from "./components/Layout/Layout";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
+import DashboardView from "./views/DashboardView/DashboardView";
+import TransactionsView from "./views/TransactionsView/TransactionsView";
+import ReportsView from "./views/ReportsView/ReportsView";
+import RootLayout from "./layouts/RootLayout";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index path="" element={<DashboardView />} />
+      <Route path="transakcje" element={<TransactionsView />} />
+      <Route path="raporty" element={<ReportsView />} />
+    </Route>
+  )
+);
 
 const App = () => {
   return (
     <>
-      <Layout />
+      <RouterProvider router={router} />
     </>
   );
 };
