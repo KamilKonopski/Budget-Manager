bc397088dd54c432d5db3700ceeadbeb9b6f3f2a
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2025-03-14 00:55:02 +0100

    add burger menu and page layout

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,15 +1,28 @@
+import { useState } from "react";
 import { Outlet } from "react-router-dom";
 
 import Footer from "../components/Footer/Footer";
-import Header from "../components/Header/Header";
 import SideBar from "../components/SideBar/SideBar";
 
+import burgerMenu from "../assets/burger-menu.png";
+
 const RootLayout = () => {
+  const [asideIsVisible, setAsideIsVisible] = useState<boolean>(true);
+
   return (
-    <div className="bg-background text-text min-h-screen font-roboto grid grid-cols-[350px_1fr] grid-rows-[auto_1fr_auto]">
-      <Header />
-      <SideBar />
-      <main>
+    <div
+      className="bg-background overflow-x-hidden w-full h-screen text-text min-h-screen font-roboto
+     grid grid-cols-[350px_1fr] grid-rows-[1fr_auto] max-xl:grid-cols-[250px_1fr]
+      max-lg:grid-cols-[200px_1fr] max-md:flex max-md:flex-col"
+    >
+      <SideBar asideIsVisible={asideIsVisible} />
+      <main className="max-md:order-first max-md:grow">
+        <div
+          onClick={() => setAsideIsVisible((prev) => !prev)}
+          className="w-10 h-10 cursor-pointer max-md:hidden"
+        >
+          <img className="w-full" src={burgerMenu} alt="menu bar" />
+        </div>
         <Outlet />
       </main>
       <Footer />
@@ -18,9 +31,3 @@ const RootLayout = () => {
 };
 
 export default RootLayout;
-
-// grid-template-areas:
-//     "header header"
-//     "menu content"
-//     "footer footer";
-//   grid-template-columns: 1fr 3fr;
