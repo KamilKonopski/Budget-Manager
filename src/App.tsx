a7c57915250a71d2738ec5c0c005b8755b84f3a5
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2025-03-21 16:06:36 +0100

    add setings path

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import DashboardView from "./views/DashboardView.tsx";
 import TransactionsView from "./views/TransactionsView";
 import RaportsView from "./views/RaportsView.tsx";
 import RootLayout from "./layouts/RootLayout";
+import SettingsView from "./views/SettingsView.tsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -16,6 +17,7 @@ const router = createBrowserRouter(
       <Route index path="" element={<DashboardView />} />
       <Route path="transactions" element={<TransactionsView />} />
       <Route path="raports" element={<RaportsView />} />
+      <Route path="settings" element={<SettingsView />} />
     </Route>
   )
 );
