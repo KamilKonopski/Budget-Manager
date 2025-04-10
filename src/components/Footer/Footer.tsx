8769e0567595a090310e68df8ff10fdabde7c600
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2025-04-10 20:20:15 +0200

    remove year

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -8,7 +8,7 @@ const Footer = () => {
           target="_blank"
           href="https://github.com/KamilKonopski"
         >
-          Kamil Konopski 2025
+          Kamil Konopski
         </a>
       </span>
     </footer>
